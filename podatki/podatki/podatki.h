7bb12955673f5ee1ef340c9441813934715fefdd
Wojciech Korniak <w.korniak@quickersim.com>
2022-04-28 18:51:00 +0200

    stworzono szkielet pliku header z klasami

---
+++ podatki/podatki/podatki.h
@@ -0,0 +1,10 @@
+#pragma once
+class Urzad {};
+class Faktura {};
+class Kwota {};
+class Osoba {};
+class OsobaFizyczna : public Osoba {};
+class OsobaPrawna : public Osoba {};
+class Dzialalnosc {};
+class Podatek {};
+class Kontener {};
