aa047e6cdc54f52d24d12970e7c1a918b1ab24d4
Wojciech Korniak <w.korniak@quickersim.com>
2022-05-04 17:32:55 +0200

    implementacja faktury i kwoty

--- podatki/podatki/podatki.h
+++ podatki/podatki/podatki.h
@@ -1,10 +1,52 @@
 #pragma once
+enum WALUTA {
+	PLN = 100,
+	EUR = 470,
+	USD = 450,
+};
+
 class Urzad {};
-class Faktura {};
-class Kwota {};
+
+class Kwota {
+private:
+	double wartosc;
+	WALUTA waluta;
+public:
+	Kwota(const double& war = 0, const WALUTA& wal = PLN) : wartosc(war), waluta(wal) {};
+	double get() const noexcept {
+		return wartosc * waluta / 100;
+	}
+};
+
+class Faktura {
+private:
+	static int liczbaID;
+	int ID;
+	int osoba;
+	Kwota kwota;
+public:
+	Faktura(const int& os, const Kwota& kw) : osoba(os), kwota(kw) {
+		ID = liczbaID;
+		liczbaID++;
+	};
+	double get() const noexcept {
+		return kwota.get();
+	}
+	int getID() const noexcept {
+		return ID;
+	}
+};
+
+int Faktura::liczbaID = 1;
+
 class Osoba {};
+
 class OsobaFizyczna : public Osoba {};
+
 class OsobaPrawna : public Osoba {};
+
 class Dzialalnosc {};
+
 class Podatek {};
+
 class Kontener {};
