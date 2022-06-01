192f377fd0c6de3e23c14ea9ddb724714e1d175c
Wojciech Korniak <w.korniak@quickersim.com>
2022-06-01 12:56:39 +0200

    dodanie klasy urzad i podzielenie klas na osobne pliki

---
+++ podatki/podatki/faktura.cpp
@@ -0,0 +1,89 @@
+#include "pch.h"
+#include "faktura.h"
+
+Kwota::Kwota(std::istream& input, std::ostream& output) {
+	output << "Podaj kwote:" << std::endl;
+	input >> wartosc;
+	output << "Podaj znak waluty:" << std::endl;
+	input >> waluta;
+	output << "Podaj kurs:" << std::endl;
+	input >> kurs;
+}
+
+double Kwota::podaj() const noexcept {
+	return wartosc * kurs;
+}
+
+std::ostream& operator<<(std::ostream& o, const Kwota& kwota) {
+	o << kwota.wartosc << kwota.waluta;
+	return o;
+}
+
+Faktura::Faktura(const int& os, const Kwota& kw) : osoba(os), kwota(kw) {
+	ID = liczbaID;
+	liczbaID++;
+};
+
+
+Faktura::Faktura(std::istream& input, std::ostream& output) {
+	ID = liczbaID;
+	liczbaID++;
+	output << "Podaj ID osoby:" << std::endl;
+	input >> osoba;
+	Kwota kw(input, output);
+	kwota = kw;
+};
+
+double Faktura::podajKwote() const noexcept {
+	return kwota.podaj();
+}
+
+int Faktura::podajID() const noexcept {
+	return ID;
+}
+
+int Faktura::podajOsobe() const noexcept {
+	return osoba;
+}
+
+void Faktura::pokaz(std::ostream& output) const noexcept {
+	output << "Faktura nr " << ID << std::endl;
+	output << "Osoba ID " << osoba << std::endl;
+	output << "Kwota " << kwota << std::endl;
+}
+
+void Faktura::edytuj(std::istream& input, std::ostream& output) noexcept {
+	output << "Wybierz co chcesz edytowac : " << std::endl;
+	output << "1. ID osoby" << std::endl;
+	output << "2. Kwote" << std::endl;
+	int wybor;
+	input >> wybor;
+	switch (wybor) {
+	case 1:
+		int nowaOsoba;
+		output << "Podaj nowy numer ID osoby :" << std::endl;
+		input >> nowaOsoba;
+		osoba = nowaOsoba;
+		this->pokaz(output);
+		break;
+	case 2:
+		double wartosc, kurs;
+		std::string waluta;
+		output << "Podaj nowa kwote" << std::endl;
+		output << "Wartosc" << std::endl;
+		input >> wartosc;
+		output << "Waluta" << std::endl;
+		input >> waluta;
+		output << "Kurs" << std::endl;
+		input >> kurs;
+		kwota = *new Kwota(wartosc, waluta, kurs);
+		this->pokaz(output);
+		break;
+	}
+}
+
+bool Faktura::operator==(const Faktura& f) const noexcept {
+	return ID == f.ID;
+}
+
+int Faktura::liczbaID = 1;
---
+++ podatki/podatki/osoba.cpp
@@ -0,0 +1,185 @@
+#include "pch.h"
+#include "osoba.h"
+
+OsobaFizyczna::OsobaFizyczna(std::istream& input, std::ostream& output)
+{
+	ID = liczbaID;
+	liczbaID++;
+	edytujImie(input, output);
+	edytujNazwisko(input, output);
+	edytujPesel(input, output);
+	edytujAdres(input, output);
+	edytujRodzajDzialalnosci(input, output);
+}
+
+int Osoba::podajID() const noexcept
+{
+	return ID;
+}
+
+int Osoba::podajDzialalnosc() const noexcept
+{
+	return rodzaj_dzialalnosci;
+}
+
+std::string Osoba::podajAdres() const noexcept
+{
+	return adres;
+}
+
+void Osoba::edytujAdres(std::istream& input, std::ostream& output)
+{
+	output << "Podaj adres: ";
+	input >> adres;
+}
+
+void Osoba::edytujRodzajDzialalnosci(std::istream& input, std::ostream& output)
+{
+	output << "Wybierz rodzaj dzialalnosci.";
+	//output wektora dzialalnosci?
+	input >> rodzaj_dzialalnosci;
+}
+
+std::string OsobaFizyczna::podajImie() const noexcept
+{
+	return imie;
+}
+
+std::string OsobaFizyczna::podajNazwisko() const noexcept
+{
+	return nazwisko;
+}
+
+std::string OsobaFizyczna::podajPesel() const noexcept
+{
+	return pesel;
+}
+
+void OsobaFizyczna::pokaz(std::ostream& output)
+{
+	output << "Imie i nazwisko : " << imie << " " << nazwisko << std::endl
+		<< "PESEL: " << pesel << std::endl
+		<< "Adres: " << adres << std::endl
+		<< "Rodzaj dzialalnosci: " << rodzaj_dzialalnosci << std::endl;
+}
+
+void OsobaFizyczna::edytujImie(std::istream& input, std::ostream& output)
+{
+	output << "Podaj imie: ";
+	input >> imie;
+}
+
+void OsobaFizyczna::edytujNazwisko(std::istream& input, std::ostream& output)
+{
+	output << "Podaj nazwisko: ";
+	input >> nazwisko;
+}
+
+void OsobaFizyczna::edytujPesel(std::istream& input, std::ostream& output)
+{
+	output << "Podaj pesel: ";
+	input >> pesel;
+}
+
+void OsobaFizyczna::edytuj(std::istream& input, std::ostream& output)
+{
+	output << "Wybierz co chcesz edytowac : " << std::endl;
+	output << "1. Imie" << std::endl;
+	output << "2. Nazwisko" << std::endl;
+	output << "3. Pesel" << std::endl;
+	output << "4. Adres" << std::endl;
+	output << "5. Rodzaj dzialalnosci" << std::endl;
+	int wybor;
+	input >> wybor;
+	switch (wybor)
+	{
+	case 1:
+		edytujImie(input, output);
+		break;
+	case 2:
+		edytujNazwisko(input, output);
+		break;
+	case 3:
+		edytujPesel(input, output);
+		break;
+	case 4:
+		edytujAdres(input, output);
+		break;
+	case 5:
+		edytujRodzajDzialalnosci(input, output);
+		break;
+	}
+
+}
+
+OsobaPrawna::OsobaPrawna(std::istream& input, std::ostream& output)
+{
+	ID = liczbaID;
+	liczbaID++;
+	edytujNazwe(input, output);
+	edytujNIP(input, output);
+	edytujAdres(input, output);
+	edytujRodzajDzialalnosci(input, output);
+}
+
+std::string OsobaPrawna::podajNazwe() const noexcept
+{
+	return nazwa;
+}
+
+std::string OsobaPrawna::podajNIP() const noexcept
+{
+	return NIP;
+}
+
+void OsobaPrawna::pokaz(std::ostream& output)
+{
+	output << "Nazwa: " << nazwa << std::endl
+		<< "NIP: " << NIP << std::endl
+		<< "Adres: " << adres << std::endl
+		<< "Rodzaj dzialalnosci: " << rodzaj_dzialalnosci << std::endl;
+}
+
+void OsobaPrawna::edytuj(std::istream& input, std::ostream& output)
+{
+	output << "Wybierz co chcesz edytowac : " << std::endl;
+	output << "1. Nazwe" << std::endl;
+	output << "2. NIP" << std::endl;
+	output << "3. Adres" << std::endl;
+	output << "4. Rodzaj dzialalnosci" << std::endl;
+	int wybor;
+	input >> wybor;
+	switch (wybor)
+	{
+	case 1:
+		edytujNazwe(input, output);
+		break;
+	case 2:
+		edytujNIP(input, output);
+		break;
+	case 3:
+		edytujAdres(input, output);
+		break;
+	case 4:
+		edytujRodzajDzialalnosci(input, output);
+		break;
+	}
+}
+
+void OsobaPrawna::edytujNazwe(std::istream& input, std::ostream& output)
+{
+	output << "Podaj nazwe osoby prawnej: ";
+	input >> nazwa;
+}
+
+void OsobaPrawna::edytujNIP(std::istream& input, std::ostream& output)
+{
+	output << "Podaj NIP: ";
+	input >> NIP;
+}
+
+bool Osoba::operator==(const Osoba& o) const noexcept {
+	return ID == o.ID;
+}
+
+int Osoba::liczbaID = 1;
